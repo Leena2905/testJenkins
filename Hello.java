e290f55689bd1e2a0bf868b47a72ff63c8aba931
Parimala <163733850+Leena2905@users.noreply.github.com>
2026-02-16 11:07:38 +0530

    add result calculation
    Fix typo in 'Syatem' and correct addition operation.

--- Hello.java
+++ Hello.java
@@ -1,5 +1,8 @@
 class Hello{
     public static void main(String args[]){
         Syatem.out.println("Hello world!!!");
+        int a=10;
+        int b=5;
+        System.out.println("Result:"+(a+b));
     }
 }
